b71e7418b2cf16ee86d7d4e009f5bd7f35f6327e
Punit Sharma (Anuj) <46415431+0xAnuj@users.noreply.github.com>
2024-08-14 14:43:16 +0530

    Update run.py

--- run.py
+++ run.py
@@ -135,7 +135,7 @@ def main():
     parser.add_argument('-w','--webhook', default=None, help='Webhook URL (default: NULL)')
     parser.add_argument('-r','--reporttype', required=True, choices=['HTML', 'XML'], help='Report type (HTML or XML)')
     parser.add_argument('--header', action='append', help='Custom headers/cookies to add to the requests (format: HeaderName:HeaderValue)')
-    parser.add_argument('--crawlonly', action='store_true', help='Set crawlonly to true in config.json')
+    parser.add_argument('--crawlonly', action='store_true', help='Perfom crawl only scan, it will save all crawled requests under ./data/')
     parser.add_argument('--socks5', action='store_true', help='Use socks5 for VPN at localhost:9090')
 
     args = parser.parse_args()
